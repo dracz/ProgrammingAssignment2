f50a4eec5950fc46885ec3a03864c81f16e9d97d
dracz <davidracz@gmail.com>
2015-06-17 13:58:30 -0700

    updated test case

--- testcache.R
+++ testcache.R
@@ -1,5 +1,9 @@
 ## test the cachematrix.R
 
 m = makeCacheMatrix(matrix(runif(10000), ncol=100))
-inv = cacheSolve(m)
+message("solving matrix inverse for the first time...")
+inv1 = cacheSolve(m)
+
+message("solving matrix inverse for the second time...")
 inv2 = cacheSolve(m)
+
